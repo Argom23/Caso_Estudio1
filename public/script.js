6081dd9f254ba2a973f1efdcb1478a647facccc2
Cesar Arce <cesar.arceg.2005@gmail.com>
2024-07-12 21:45:19 -0600

    Commit 12/7/24

--- public/script.js
+++ public/script.js
@@ -1,5 +1,35 @@
+let myNotes = [];
+
+function generateId() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
 // Función para agregar una nueva nota
 function addNote() {
+    var title = document.getElementById("title").value;
+    var content = document.getElementById("content").value;
+    var tags = document.getElementById("tags").value;
+    var tags_array = tags.split(',').map(tag => tag.trim()); // Separa etiquetas por comas y eliminar espacios
+
+    const id = generateId(); // Generar un ID único
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    var note = {
+        id: id,
+        title: title,
+        content: content,
+        tags: tags_array,
+        createdAt: createdAt,
+        updatedAt: updatedAt
+    };
+
+    myNotes.push(note);
+    
+    window.location.href = 'index.html';
+}
+
+/* function findNote() {
     var title = document.getElementById("title").value;
     var content = document.getElementById("content").value;
     var tagsS = document.getElementById("tags").value;
@@ -17,5 +47,4 @@ function addNote() {
         createdAt,
         updatedAt
     };
-}
-
+} */
